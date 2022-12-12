304aece4a57be3d605eb775ef9e921d2ba59c879
Ecmel SADIKOĞLU <sadikogluecmel@gmail.com>
2022-12-12 16:57:08 +0300

    Api crud

---
+++ Core_Proje_Api/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using Core_Proje_Api.DAL.ApiContext;
+using Core_Proje_Api.DAL.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace Core_Proje_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        Context c = new Context();
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            return Ok(c.Categories.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryByID(int id)
+        {
+            var category = c.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(category);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CategoryAdd(Category category)
+        {
+
+            c.Add(category);
+            c.SaveChanges();
+            return Created("", category);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = c.Categories.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Categories.Remove(value);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+
+        [HttpPut]
+
+        public IActionResult UpdateCategory(Category p)
+        {
+            var category = c.Categories.Find(p.CategoryID);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                category.CategoryName = p.CategoryName;
+                c.Update(category);
+                c.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
